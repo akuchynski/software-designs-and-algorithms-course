69b4224b95e06206bdea141d26cda5dfe222a05e
Andrei Kuchynski <Andrei_Kuchynski1@epam.com>
2023-02-22 23:16:54 +0300

    task1: implement all classes

---
+++ building-blocks-of-oop-part-1/src/Point.ts
@@ -0,0 +1,33 @@
+export class Point {
+  private readonly x: number;
+  private readonly y: number;
+
+  constructor();
+  constructor(x: number, y: number);
+  constructor(x: number = 0, y: number = 0) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public distance();
+  public distance(otherPoint: Point);
+  public distance(x, y);
+  public distance(otherPointOrX: Point | number = 0, y: number = 0): number {
+    let x2: number;
+    let y2: number;
+
+    if (otherPointOrX instanceof Point) {
+      x2 = otherPointOrX.x;
+      y2 = otherPointOrX.y;
+    } else {
+      x2 = otherPointOrX;
+      y2 = y;
+    }
+
+    return Math.sqrt(Math.pow(x2 - this.x, 2) + Math.pow(y2 - this.y, 2));
+  }
+
+  public toString(): string {
+    return `(${this.x}, ${this.y})`;
+  }
+}
---
+++ building-blocks-of-oop-part-1/src/Shape.ts
@@ -0,0 +1,48 @@
+import { Point } from "./Point";
+
+export abstract class Shape {
+  protected readonly color: string;
+  protected readonly filled: boolean;
+  protected readonly points: Point[];
+
+  abstract getType(): string;
+
+  constructor(points: Point[]);
+  constructor(points: Point[], color: string, filled: boolean);
+  constructor(points: Point[], color: string = "green", filled: boolean = true) {
+    if (!points || points.length < 3) {
+      throw Error("Should receives at least 3 points");
+    }
+
+    this.points = points;
+    this.color = color;
+    this.filled = filled;
+  }
+
+  public toString(): string {
+    return (
+      `A Shape with color of ${this.color} and ${this.filled ? "filled" : "not filled"}. ` +
+      `Points: ${this.points.reduce((acc, point, index, arr) => {
+        if (index === arr.length - 1) {
+          return `${acc}${point.toString()}.`;
+        } else {
+          return `${acc}${point.toString()}, `;
+        }
+      }, "")}`
+    );
+  }
+
+  public getPerimeter(): number {
+    let perimeter: number = 0;
+
+    this.points.forEach((point, index, arr) => {
+      if (index === arr.length - 1) {
+        return (perimeter += point.distance(arr[0]));
+      }
+
+      return (perimeter += point.distance(arr[index + 1]));
+    });
+
+    return perimeter;
+  }
+}
